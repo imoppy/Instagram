7e1c0ad0452ef010505edd51fb421ca8b539acd3
jun.wakahara <wakahara_jun@kakaku.com>
2022-04-06 17:26:45 +0900

    コメント投稿機能追加

---
+++ Instagram/CommentViewController.swift
@@ -0,0 +1,49 @@
+//
+//  CommentViewController.swift
+//  Instagram
+//
+//  Created by 若原 旬 on 2022/04/06.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class CommentViewController: UIViewController {
+    var postId: String!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.layer.borderWidth = 0.5
+        textView.layer.cornerRadius = 10.0
+        textView.layer.masksToBounds = true
+    }
+
+    @IBAction func handleCommentButton(_ sender: Any) {
+        if textView.text == nil {
+            SVProgressHUD.showError(withStatus: "コメントを入力してください")
+            return
+        }
+
+        if Auth.auth().currentUser?.uid == nil {
+            SVProgressHUD.showError(withStatus: "ログインしてください")
+            return
+        }
+        
+        SVProgressHUD.show()
+
+        let postRef = Firestore.firestore().collection(Const.PostPath).document(postId)
+        postRef.updateData(["comments": FieldValue.arrayUnion([textView.text])])
+
+        SVProgressHUD.showSuccess(withStatus: "投稿しました")
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func handleCancelButton(_ sender: Any) {
+        // 画面を閉じる
+        self.dismiss(animated: true, completion: nil)
+    }
+}
--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -70,13 +70,14 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         cell.setPostData(postArray[indexPath.row])
 
         // セル内のボタンのアクションをソースコードで設定する
-        cell.likeButton.addTarget(self, action:#selector(handleButton(_:forEvent:)), for: .touchUpInside)
+        cell.likeButton.addTarget(self, action:#selector(handleLikeButton(_:forEvent:)), for: .touchUpInside)
+        cell.commentButton.addTarget(self, action:#selector(handleCommentButton(_:forEvent:)), for: .touchUpInside)
 
         return cell
     }
 
-    // セル内のボタンがタップされた時に呼ばれるメソッド
-    @objc func handleButton(_ sender: UIButton, forEvent event: UIEvent) {
+    // セル内のいいねボタンがタップされた時に呼ばれるメソッド
+    @objc func handleLikeButton(_ sender: UIButton, forEvent event: UIEvent) {
         print("DEBUG_PRINT: likeボタンがタップされました。")
 
         // タップされたセルのインデックスを求める
@@ -102,5 +103,23 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
             let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
             postRef.updateData(["likes": updateValue])
         }
+    }
+
+    // セル内のコメントボタンがタップされた時に呼ばれるメソッド
+    @objc func handleCommentButton(_ sender: UIButton, forEvent event: UIEvent) {
+        print("DEBUG_PRINT: commentボタンがタップされました。")
+
+        // タップされたセルのインデックスを求める
+        let touch = event.allTouches?.first
+        let point = touch!.location(in: self.tableView)
+        let indexPath = tableView.indexPathForRow(at: point)
+
+        // 配列からタップされたインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+
+        // コメント投稿画面を開く
+        let commentViewController = self.storyboard?.instantiateViewController(withIdentifier: "Comment") as! CommentViewController
+        commentViewController.postId = postData.id
+        self.present(commentViewController, animated: true, completion: nil)
     }
 }
